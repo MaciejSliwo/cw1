cef2fe6b2556ba24bb2fbcff2fb04af77fbf781d
MaciejSliwo <32269589+MaciejSliwo@users.noreply.github.com>
2017-09-25 17:02:36 +0200

    Nowa zmienna 2

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
-    string imie = "Maciej";
-    string nazwisko = "Sliwonik";
-    int rok_urodzenia = 1996;
-    int aktualny_rok = 2017;
+    string imie;
+    string nazwisko;
+    int rok_urodzenia;
+    int aktualny_rok;
 
-    cout<<imie<<" ";
-    cout<<nazwisko;
-    cout<<" mam ";
-    cout<<aktualny_rok - rok_urodzenia;
+    cout<<"podaj imie"<<endl;
+    cin>>imie;
+    cout<<"Podaj nazwisko"<<endl;
+    cin>>nazwisko;
+    cout<<"Podaj rok urodzenia"<<endl;
+    cin>>rok_urodzenia;
+    cout<<"Podaj aktualny rok"<<endl;
+    cin>>aktualny_rok;
 
+    cout<<imie<<" "<<nazwisko<<" mam lat "<<aktualny_rok-rok_urodzenia;
+
+    system("pause");
     return 0;
 }
